3f3c4286f785f66c7adc02b7b7e444faa0e96d36
ronansenati <ronansena_ti@yahoo.com.br>
2019-05-26 21:23:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-
-		Departament obj = new Departament(1,"Book");
-		System.out.println(obj);
+		Departament obj = new Departament(1, "Book");
+		Seller seller = new Seller(1,"Bob","bob@gmail.com", new Date(),3000.0, obj);
+		System.out.println(seller);
 
 	}
 
